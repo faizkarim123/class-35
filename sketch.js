5674a067097365a5e1e0bda24db368c8ad6ae43d
faizkarim123 <77217146+faizkarim123@users.noreply.github.com>
2022-02-22 17:45:40 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+var hypnoticBall, database;
+var position
+
+function setup(){
+    databse = firebase.database();
+    createCanvas(500,500);
+    hypnoticBall = createSprite(250,250,10,10);
+    hypnoticBall.shapeColor = "red";
+var hypnoticBallPosition = database.ref("ball/position");
+hypnoticBallPosition.on("value",readPosition);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+   databse.ref("ball/position").set({
+    "x":position.x + x, 
+    "y":position.y + y
+   })
+}
+
+function readPosition(data){
+    position = data.val();
+    hypnoticBall.x = position.x;
+    hypnoticBall.y = position.y;
+}
